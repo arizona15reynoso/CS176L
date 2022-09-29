7bc865709cca1ad2f8b72496887e3bfd0619b808
arizona15reynoso <98927151+arizona15reynoso@users.noreply.github.com>
2022-09-29 15:57:29 -0400

    MicrowaveTester

---
+++ MicrowaveTester.java
@@ -0,0 +1,16 @@
+
+public class MicrowaveTester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			Microwave microwave = new Microwave();
+
+			microwave.increaseTime();
+			microwave.increaseTime();
+			microwave.switchPower();
+			microwave.start();
+			microwave.reset();
+		
+		}
+
+	}
